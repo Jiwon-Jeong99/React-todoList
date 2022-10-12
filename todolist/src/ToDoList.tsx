c357f5bd96d8400667578e36a16f2c7ae26b5b71
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-10-12 23:51:25 +0900

    form

--- todolist/src/ToDoList.tsx
+++ todolist/src/ToDoList.tsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 function ToDoList() {
-  return null;
+  const [toDo, setToDo] = useState("");
+  //타입스크립트할 때만 적는거
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setToDo(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(toDo);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default ToDoList;
