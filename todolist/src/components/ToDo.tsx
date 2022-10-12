927fc52e77fb6d663a332f46581b9dc866b2748f
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-10-13 02:06:13 +0900

    new&old todo

--- todolist/src/components/ToDo.tsx
+++ todolist/src/components/ToDo.tsx
@@ -1,12 +1,32 @@
-import { IToDo } from "../atoms";
+import { IToDo, toDoState } from "../atoms";
+import { useSetRecoilState } from "recoil";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (newCategory: IToDo["category"]) => {
+    //todo 수정 - 현재 todo의 id값을 찾기(index로 0,1,2...)
+    setToDos((oldToDos) => {
+      //props로부터 오는 id와 toDo.id가 같은지 확인
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const oldToDo = oldToDos[targetIndex];
+      const newToDo = { text, id, newCategory };
+      console.log(oldToDo, newToDo);
+      return oldToDos;
+    });
+  };
   return (
     <li>
-        <span>{text}</span>
-        <button>To Do</button>
-        <button>Doing</button>
-        <button>Done</button>
+      <span>{text}</span>
+      {/* onclick하면 인자가 넘어갈 수 있도록 함수형태로 */}
+      {category !== "TO_DO" && (
+        <button onClick={() => onClick("TO_DO")}>To Do</button>
+      )}
+      {category !== "DOING" && (
+        <button onClick={() => onClick("DOING")}>Doing</button>
+      )}
+      {category !== "DONE" && (
+        <button onClick={() => onClick("DONE")}>Done</button>
+      )}
     </li>
   );
 }
--- todolist/src/components/ToDoList.tsx
+++ todolist/src/components/ToDoList.tsx
@@ -5,8 +5,9 @@ import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-  // 값을 가져오기만 하면 됨
+  // 입력해서 atom에 저장된 값을 가져오기만 하면 됨
   const toDos = useRecoilValue(toDoState);
+  console.log(toDos);
   return (
     <div>
       <h1>To Dos</h1>
